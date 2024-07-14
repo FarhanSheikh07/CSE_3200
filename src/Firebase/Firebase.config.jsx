eb0ec890af5794262b3dd1d11caeb8af1e4bbac1
FarhanSheikh07 <115068467+FarhanSheikh07@users.noreply.github.com>
2024-07-14 09:43:12 +0600

    Final Files

---
+++ src/Firebase/Firebase.config.jsx
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAQBbb8736TRzRbed2KIVgyHY17uejIwkU",
+  authDomain: "ruet-7390c.firebaseapp.com",
+  projectId: "ruet-7390c",
+  storageBucket: "ruet-7390c.appspot.com",
+  messagingSenderId: "713604546574",
+  appId: "1:713604546574:web:8e6cb9378ea80674623c5f"
+};
+
+
+export const app = initializeApp(firebaseConfig);
